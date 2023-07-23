281a03b50b161b84acc8849c42bf05be54cb72a3
SurapureddySusmitha21 <surapureddysusmitha@gmail.com>
2023-07-23 12:31:12 +0000

    first commitcongratscard

---
+++ index.js
@@ -0,0 +1,24 @@
+const element = (
+  <div class="bg_container">
+    <h1 class="heading">Congratulations</h1>
+    <div class="second_container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        class="image_size"
+      />
+      <h1 class="kiran_heading">Kiran V</h1>
+      <p class="kiran_paragraph">
+        Vishnu institute of computer education and Technology.Bhimavaram
+      </p>
+
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        class="image_size"
+      />
+    </div>
+  </div>
+
+  // Write your code here.
+);
+
+ReactDOM.render(element, document.getElementById("root"));
